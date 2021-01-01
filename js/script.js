d2dfdd893d4667829527c51ab770e2874d95c656
Rowin Ruizendaal <rowin@medialab.co>
2021-01-01 23:27:47 +0100

    Changed some styling

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
-const logo = document.querySelectorAll('#logo path')
+const paragraph = document.querySelector('.container p')
 
-console.log(logo)
+const interval = setInterval(getTime, 1000);
+
+
+
+
+
+function getTime () {
+    const date = new Date();
+    let curr_hour = date.getHours();
+    let curr_min = date.getMinutes();
+    let curr_sec = date.getSeconds();
+
+    if (curr_sec < 10) {
+        curr_sec = `0${curr_sec}`
+    } else if ( curr_min < 10) {
+        curr_min = `0${curr_min}`
+    } else if (curr_hour < 10 ){
+        curr_hour = `0${curr_hour}`
+    } else {
+        // Nothing here :D
+    }
+
+    paragraph.innerHTML = `${curr_hour}:${curr_min}:${curr_sec}`
+}
